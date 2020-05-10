03845a79ae0805bc047e7964d476256258469c0d
Daddyawesome <sablay296@gmail.com>
2020-05-10 21:51:48 +0000

    fixing login and register

--- application/forms.py
+++ application/forms.py
@@ -2,13 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
-class LoginForm(Flaskform):
+class LoginForm(FlaskForm):
     email       = StringField("Email", validators=[DataRequired()])
     password    = StringField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit      = SubmitField("Login")
 
-class RegisterForm(Flaskform):
+class RegisterForm(FlaskForm):
     email               = StringField("Email", validators=[DataRequired()])
     password            = StringField("Password", validators=[DataRequired()])
     password_confirm    = StringField("Confirm Password", validators=[DataRequired()])
