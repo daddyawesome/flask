7c5b97dd5b20a1b52fcf3e50b9c40f387a4d1719
Daddyawesome <sablay296@gmail.com>
2020-05-10 20:45:23 +0000

    adding models

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
 
-    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' }
-
-
+    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment',
+                    'host': 'mongodb+srv://test:test@cluster0-13gle.gcp.mongodb.net/UTA_Enrollment?retryWrites=true&w=majority' }
     
