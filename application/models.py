5518e045fff8951d09388aad4c6f5ff741e83cb5
Daddyawesome <sablay296@gmail.com>
2020-05-31 05:31:27 +0000

    registration and classes connected to db

--- application/models.py
+++ application/models.py
@@ -16,12 +16,12 @@ class User(db.Document):
         return check_password_hash(self.password, password)  
         
 class Course(db.Document):
-    course_id       =   db.StringField( max_length=10, unique=True )
-    title           =   db.StringField( max_length=100 )
-    descritption    =   db.StringField( max_length=255 )
-    credits         =   db.IntField()
-    term            =   db.StringField( max_length=25 )
+    courseID       =   db.StringField( max_length=10, unique=True )
+    title          =   db.StringField( max_length=100 )
+    description    =   db.StringField( max_length=255 )
+    credits        =   db.IntField()
+    term           =   db.StringField( max_length=25 )
 
 class Enrollment(db.Document):
-    user_id         =   db.IntField()
-    course_id       =   db.StringField( max_length=10)
+    user_id        =   db.IntField()
+    courseID       =   db.StringField( max_length=10)
